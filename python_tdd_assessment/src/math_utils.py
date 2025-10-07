6217ad1f719a3be1d900d2adb74f2c53810bedea
Thabo Mafa <thtshjhb025@student.wethinkcode.co.za>
2025-10-07 13:13:23 +0200

    Added math-utils

--- python_tdd_assessment/src/math_utils.py
+++ python_tdd_assessment/src/math_utils.py
@@ -5,16 +5,37 @@
 
 def add(a, b):
     # TODO: Return the sum of a and b
-    pass
+    print(add)
+   # def add(self):
+     #   self.assertEqual(add(a + b), a + b)
+
+add = "a+b" 
+input("add a + b :")
+print("Your answer is =" + add)
+
 
 def subtract(a, b):
     # TODO: Return the difference between a and b
-    pass
+   # def add(self):
+     #   self.assertEqual(subtract(a - b), a - b)
+ subtract= "a-b" 
+input("subtract a - b :")
+print("Your answer is =" + subtract)
+     
 
 def multiply(a, b):
     # TODO: Return the product of a and b
-    pass
-
+ # def multiply(self):
+     #   self.assertEqual(add(a * b), ab)
+     
+ multiply = "ab" 
+input("multiply a * b :")
+print("Your answer is =" + multiply)
+        
 def divide(a, b):
     # TODO: Return the quotient of a / b, handle division by zero with ValueError
-    pass
+      # def divide(self):
+      # self.assertEqual(add(a / b), a/b)
+ divide = "a/b" 
+input("multiply a / b :")
+print("Your answer is =" + divide)
