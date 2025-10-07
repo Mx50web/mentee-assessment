23d1570218652f6f24cc577703d7614b4382570a
Thabo Mafa <thtshjhb025@student.wethinkcode.co.za>
2025-10-07 12:08:18 +0200

    Added mentee-assessment$ ls

---
+++ python_tdd_assessment/tests/test_utils.py
@@ -0,0 +1,19 @@
+
+import unittest
+
+class testCount(unittest.TestCase):
+    def test_count_vowels(self):
+        self.assertEqual(count_vowels("abigail", "mahlodi"), "zilla", "samson" , "bilgates")
+        
+def get_max(numbers):
+    if not numbers:
+        return None
+    return max(numbers)
+
+def count_vowels(word):
+    vowels = 'aeiou'
+    return sum(1 for ch in word.lower() if ch in vowels)
+
+if __name__ == "__main__":
+    unittest.main
+
